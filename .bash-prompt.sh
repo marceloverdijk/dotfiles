501e4724d5f8fa7b66ec15ff2657e9a277426a75
Marcel Overdijk <marcel@overdijk.me>
2014-10-25 23:44:37 +0200

    refining working dir output

--- .bash-prompt.sh
+++ .bash-prompt.sh
@@ -15,36 +15,40 @@ GREEN="\[\e[0;32m\]"
 COLOREND="\[\e[00m\]"
 
 working_directory() {
-  # see http://stackoverflow.com/questions/26554713/how-to-truncate-working-directory-in-prompt-to-show-first-and-last-folder
-  cwd=`pwd | awk -F/ -v "n=$(tput cols)" -v "h=^$HOME" '{sub(h,"~");n=0.7*n;b=$1"/"$2} length($0)<=n || NF==3 {print;next;} NF>3{b=b"/../"; e=$NF; n-=length(b $NF); for (i=NF-1;i>3 && n>length($i)+1;i--) e=$i"/"e;} {print b e;}'`
+  max_length=$(expr `tput cols` - 40)
+  dir=`pwd`
+  if [[ `pwd` =~ ^"$HOME"(/|$) ]]; then
+    dir="~${dir#$HOME}"
+  fi
+  if [[ $max_length -lt 30 ]]; then
+    cwd=${PWD##*/}
+  elif [[ $max_length -gt ${#dir} ]]; then
+    cwd=$dir
+  else
+    # see http://stackoverflow.com/questions/26554713/how-to-truncate-working-directory-in-prompt-to-show-first-and-last-folder
+    # cwd=`pwd | awk -F/ -v "n=$(tput cols)" -v "h=^$HOME" '{sub(h,"~");n=n-40;b=$1"/"$2} length($0)<=n || NF==3 {print;next;} NF>3{b=b"/../"; e=$NF; n-=length(b $NF); for (i=NF-1;i>3 && n>length($i)+1;i--) e=$i"/"e;} {print b e;}'`
 
-  # dir=`pwd`
-  # if [[ `pwd` =~ ^"$HOME"(/|$) ]]; then
-  #   dir="~${dir#$HOME}"
-  # fi
-  # cwd=""
-  # if [[ $dir == "/" ]]; then
-  #   cwd+="/"
-  # fi
-  #
-  # while IFS='/' read -ra subdirs; do
-  #   for subdir in "${subdirs[@]}"; do
-  #     if [[ $subdir != "~" ]]; then
-  #       cwd+="/"
-  #     fi
-  #     cwd+=$subdir
-  #   done
-  # done <<< "$dir"
+    while IFS='/' read -ra subdirs; do
+      length=${#subdirs[@]}
 
+      # refactor below
+      for (( i=${length}; i>0; i-- )); do
+        if [[ $i == $length && $i != 1 ]]; then
+          cwd="/${subdirs[$i-1]}"
+        elif [[ $i == 1 ]]; then
+          if [[ ${subdirs[$i-1]} == "~" ]]; then
+            cwd="~$cwd"
+          else
+            cwd="/${subdirs[$i-1]}$cwd"
+          fi
+        else
+          cwd="/${subdirs[$i-1]:0:1}$cwd"
+        fi
+      done
+      # refactor below end
 
-
-  # for subdir in $(echo $dir | tr "/" "\n"); do
-  #   if [[ $subdir != "~" ]]; then
-  #     cwd+="/"
-  #   fi
-  #   cwd+=${subdir:0:1}
-  # done
-  # cwd+=${subdir:1}
+    done <<< "$dir"
+  fi
   echo "${YELLOW}$cwd${COLOREND} "
 }
 
